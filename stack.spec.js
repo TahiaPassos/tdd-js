7f8886448d607c6157f82c843532ac456157c503
Tahiana Passos1 <tahiana.passos1@ibm.com>
2021-11-05 14:13:58 -0300

    Stack começa vazia, com tamanho 0 e não vazia após o 'push'. Três condições atendidas

--- stack.spec.js
+++ stack.spec.js
@@ -1,7 +1,13 @@
-let stack = {
-  isEmpty: () => true,
-  size: () => 0
-};
+const stackFactory = () => {
+  let empty = true;
+  return {
+  isEmpty: () => empty,
+  size: () => 0,
+  push: () => { empty = false;}
+  }
+}
+
+let stack = stackFactory();
 
 describe('canary test for stack task', () => {
     it('show the infrastructure works', () => {
@@ -17,13 +23,24 @@ describe('a stack', () => {
     expect(stack.size()).toBe(0);
   });
 
-  it.todo('is not empty when pushed');
+  it('is not empty when pushed', () => {
+    stack.push();
+    expect(stack.isEmpty()).toBe(false);
+  });
+
   it.todo('stack size is 1 when pushed');
+
   it.todo('stack is empty when pushed and popped');
+
   it.todo('stack size is 0 when pushed and popped');
+
   it.todo('throws overflow error when pushing to a stack at full capacity');
+
   it.todo('throw underflow error when popping an empty stack');
+
   it.todo('pops the same one item when pushed');
+
   it.todo('pops two items with the most recent first');
+
   it.todo('accepts only a positive capacity');
 });
