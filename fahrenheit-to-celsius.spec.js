ba2f3dba2acd479d5f7445b97a9a8c1adc0883fc
Tahiana Passos1 <tahiana.passos1@ibm.com>
2021-11-05 11:05:57 -0300

    Segundo teste implementado com failed

--- fahrenheit-to-celsius.spec.js
+++ fahrenheit-to-celsius.spec.js
@@ -1,4 +1,4 @@
-const fahrenheitToCelsiu = () => {
+const fahrenheitToCelsius = () => {
   return 0;
 }
 
@@ -10,9 +10,11 @@ describe('canary test for fahrenheit to celsiu', () => {
 
 describe('the function to convert fahrenheit to celsiu should', () => {
   it('return 0 for 32', () => {
-    expect(fahrenheitToCelsiu(32)).toBe(0);
+    expect(fahrenheitToCelsius(32)).toBe(0);
   });
-  it.todo('return 10 for 50');
+  it('return 10 for 50', () => {
+    expect(fahrenheitToCelsius(50).toBe(10));
+  });
   it.todo('return 100 for 212');
   it.todo('return -40 for -40');
   it.todo('return -273.15 for -459.67');
