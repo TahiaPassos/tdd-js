0f1c12e2dc3e43c98bc90b3850d3c1d3b50b972f
Tahiana Passos1 <tahiana.passos1@ibm.com>
2021-11-05 11:12:26 -0300

    Lógica adicionada à função

--- fahrenheit-to-celsius.spec.js
+++ fahrenheit-to-celsius.spec.js
@@ -1,19 +1,20 @@
-const fahrenheitToCelsius = () => {
-  return 0;
+const fahrenheitToCelsius = (fahrenheit) => {
+ return (fahrenheit - 32) * 5/9;
 }
 
-describe('canary test for fahrenheit to celsiu', () => {
+
+describe('canary test for fahrenheit to celsius', () => {
   it('shows the infrastructure works', ()=> {
     expect(true).toBe(true);
   })
 })
 
-describe('the function to convert fahrenheit to celsiu should', () => {
+describe('the function to convert fahrenheit to celsius should', () => {
   it('return 0 for 32', () => {
     expect(fahrenheitToCelsius(32)).toBe(0);
   });
   it('return 10 for 50', () => {
-    expect(fahrenheitToCelsius(50).toBe(10));
+    expect(fahrenheitToCelsius(50)).toBe(10);
   });
   it.todo('return 100 for 212');
   it.todo('return -40 for -40');
